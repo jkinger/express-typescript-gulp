3138721a36038f18ed104210dee46689e2d396da
Josh King <joshk@realityi.com>
2017-04-18 17:51:18 -0400

    Added Linting tools.

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import * as path from 'path';
-import * as express from 'express';
-import * as bodyParser from 'body-parser'; // Parse incoming request bodies
-import * as cookieParser from 'cookie-parser'; // Parse incoming request's cookies
-import * as morgan from 'morgan'; // HTTP request logger middleware
-import * as compression from 'compression'; // Compress response bodies for all requests
+import path from 'path';
+import express from 'express';
+import bodyParser from 'body-parser'; // Parse incoming request bodies
+import cookieParser from 'cookie-parser'; // Parse incoming request's cookies
+import logger from 'morgan'; // HTTP request logger middleware
+import compression from 'compression'; // Compress response bodies for all requests
+import errorHandler from 'errorhandler';
+import chalk from 'chalk';
 
 const app = express();
 const ROOT = path.join(path.resolve(__dirname, '../..'));
 
-/**
- * Controllers
- */
+// Controllers
 import index from './controllers/index';
 import about from './controllers/about';
 import blog from './controllers/blog';
@@ -27,10 +27,11 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // Add middleware
-app.use(cookieParser());
-app.use(bodyParser.json());
 app.use(compression());
-app.use(morgan('dev'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // Add cache control headers
 function cacheControl(req, res, next) {
@@ -40,8 +41,8 @@ function cacheControl(req, res, next) {
 }
 
 // Serve static files with cache control headers
-app.use('/styles', cacheControl, express.static(path.join(__dirname, 'styles'), {maxAge: 30}));
-app.use('/images', cacheControl, express.static(path.join(__dirname, 'images'), {maxAge: 30}));
+app.use('/styles', cacheControl, express.static(path.join(__dirname, 'styles'), { maxAge: 30 }));
+app.use('/images', cacheControl, express.static(path.join(__dirname, 'images'), { maxAge: 30 }));
 // app.use(cacheControl, express.static(path.join(ROOT, 'dist/client'), {index: false}));
 
 // Add routes
@@ -52,6 +53,11 @@ app.use('/contact', contact);
 app.use('/portfolio', portfolio);
 app.use('/services', services);
 
+// Error Handler.
+app.use(errorHandler());
+
+// Start Express server.
 const server = app.listen(app.get('port'), () => {
- console.log(`Listening on: http://localhost:${server.address().port}`);
+  console.log(`${chalk.green('✓')} App is running at http://localhost:${app.get('port')} in ${app.get('env')} mode`);
+  console.log('  Press CTRL-C to stop\n');
 });
