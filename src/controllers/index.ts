6d37d95fe8860fe4f3981590513bbeef3436fddd
Josh King <joshk@realityi.com>
2017-05-15 18:00:28 -0400

    i18next init

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,6 @@
 // Import only what we need from express
 import { Router, Request, Response } from 'express';
+import i18next from 'i18next';
 
 const index: Router = Router();
 
@@ -7,12 +8,13 @@ const index: Router = Router();
  * GET /
  * Home page.
  */
-index.get('/', (reg: Request, res: Response) => {
+index.get('/', ( req: Request, res: Response) => {
   res.render(
     'index',
     {
       title: 'Home',
-      description: 'This is the home description'
+      description: 'This is the home description',
+      myKey: req.t('myKey')
     });
 });
 
--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,8 @@ import * as logger from 'morgan'; // HTTP request logger middleware
 import * as compression from 'compression'; // Compress response bodies for all requests
 import * as errorHandler from 'errorhandler';
 import * as chalk from 'chalk';
+import i18next from 'i18next';
+import i18nextExpress from 'i18next-express-middleware'; // middleware to use i18next in express.js.
 import router from './router';
 
 const app = express();
@@ -26,6 +28,17 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// multilingual support
+i18next
+  .use(i18nextExpress.LanguageDetector)
+  .init({
+    preload: ['en', 'es']
+   });
+
+app.use(i18nextExpress.handle(i18next, {
+  removeLngFromUrl: false
+}));
 
 // Add cache control headers
 function cacheControl(req, res, next) {
